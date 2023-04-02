75874d28ec0d6041e027b4b10fe94ea5477e22a4
hkarzu97 <128447892+hkarzu97@users.noreply.github.com>
2023-04-03 03:27:59 +0600

    Add files via upload

--- message.php
+++ message.php
@@ -1,11 +1,14 @@
 <?php
-    require 'core/session.php';
-    require 'core/config.php';
-    include 'core/user_key.php';
-    //for session
-    $session=$_SESSION['email'];
-    $ref = rand (3858558,100000);$error = "";$message = "";$alpha="N S U ";
+  require 'session.php';
+  require '../core/config.php';
+  require 'dummy-key.php';
+
+  $result = mysql_query("SELECT * FROM `cmp_log`");
+  $num_rows = mysql_num_rows($result);
+
+  $eng_id = "";
  ?>
+
 <!DOCTYPE html>
 <html>
   <head>
@@ -13,121 +16,72 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>NSU CLS</title>
-    <link rel="shortcut icon" href="files/img/ico.ico">
-    <link rel="stylesheet" href="files/css/bootstrap.css">
-    <link rel="stylesheet" href="files/css/custom.css">
-    <style media="screen">
-    table{border: 0px;}
-    td{
-      padding:10px;
-      border-top: 0px solid #eee;
-      border-bottom: 0px solid #eee!important;
-      padding-left: 0px;
-      color:#0ea798;
-    }
-    input,button.log{width: 450px;}
-    </style>
+    <link rel="shortcut icon" href="../files/img/ico.ico">
+    <link rel="stylesheet" href="../files/css/bootstrap.css">
+    <link rel="stylesheet" href="../files/css/custom.css">
+
+
   </head>
   <body>
-    <div class="cover user text-center" style="height:120px;">
-      <br>
-      <h2>Add Complaints</h2>
-    </div>
-    <?php require 'nav-profile.php'; ?>
-    <div class="animated fadeIn">
-      <div class="padd">
-        <div class="col-lg-12 text-center">
-          <?php
-            $query1=mysql_query("SELECT * FROM `circle` WHERE email LIKE '%$session%'");
-            while( $arry=mysql_fetch_array($query1) ) {
-              $id=$arry['id'];
-              $name=$arry['name'];
-              $nsu_id=$arry['nsu_id'];
-              $email = $arry['email'];
-                 }
-                   if(empty($_POST)===false){
-                     $phoneno =mysql_real_escape_string($_POST['phoneno']);
-                     $subject = mysql_real_escape_string($_POST['subject']);
-                     $complain = mysql_real_escape_string($_POST['complain']);
-                     if(empty($phoneno) || empty($subject) || empty($complain)){
-                     }elseif (!preg_match("/^[0-9]*$/",$phoneno)) {
-                       $error = "Invalid Phone Number";
-                     }else{
-                       mysql_query("INSERT INTO `cmp_log` VALUES ('0','$id','$name','$nsu_id','$email','$phoneno','$subject','$complain','$ref')") or die(mysql_error());
-                       $message = "Your Complain has been Registerd";
-                       }
-                   }
-              ?>
-          <form class="" method="post" autocomplete="off">
-            <div class="container">
-              <div class="panel panel-default">
-                  <div class="panel-body">
-                      <h2>Your Refference no : &nbsp;&nbsp;&nbsp;&nbsp;<?php echo $ref;
-                      echo"<p><span class='error'>".$error."</p></span>";
-                      echo "<p><span class='message'>".$message."</p></span>";
-                      ?></h2>
-                  </div>
+
+  <?php require 'nav.php'; ?>
+
+  <div class="cover main" style="background:#40C4FF;">
+    <h1>Your Inbox,</h1>
+  </div>
+  <div class="animated fadeIn">
+
+    <div class="container">
+        <div class="col-lg-12 ">
+          <?php $result = mysql_query("SELECT * FROM `view_cmp` WHERE dummy LIKE '%$eng_session%' ")or die(mysql_error());
+            $num_rows = mysql_num_rows($result);
+            ?>
+              <div class='admin-data'>
+                Messages
+                <span class='button view' href=''><?php echo "$num_rows";?></a>
               </div>
-              <table>
-                <tr>
-                  <td class="text-left">Your Refference no</td>
-                  <td class="text-left"><div class="dis_b"><?php echo $ref;  ?></div></td>
-                </tr>
-                <tr>
-                  <td class="text-left">Your ID</td>
-                  <td class="text-left"><div class="dis_b"><?php echo $nsu_id;?></div></td>
-                </tr>
-                <tr>
-                  <td class="text-left">Your User ID</td>
-                  <td class="text-left"><div class="dis_b"><?php echo $alpha.$id;  $user_id = $id;?></div></td>
-                </tr>
-                <tr>
-                  <td class="text-left">Your Your Email ID</td>
-                  <td class="text-left"><div class="dis_b"><?php echo $email;?></div></td>
-                </tr>
-                <tr>
-                  <td class="text-left">Your Contact Number *</td>
-                  <td><input type = "text" name = "phoneno" maxlength=10 placeholder = "Your Phone number">  </td>
-                </tr>
-                <!-- New add -->
+              <br><br><br><br>
+              <br><br>
 
-                <tr>
-                  <td class="text-left">Complaint Against *</td>
-                  <td><select class="select" multiple data-mdb-clear-button="true">
-                        <option value="1">One</option>
-                        <option value="2">Two</option>
-                        <option value="3">Three</option>
-                        <option value="4">Four</option>
-                        <option value="5">Five</option>
-                      </select>
-                  </td>
-                </tr>
+              <br>
+              <h2 class="text-center"><?php echo $message; ?></h2>
+              <br><br>
 
-                <!-- New add -->
-                <tr>
-                  <td class="text-left">Your Subject *</td>
-                  <td><input type="text" name = "subject" placeholder = "Subject"></td>
-                </tr>
-                <tr>
-                  <td class="text-left">Your Complain *</td>
-                  <td><textarea name="complain" rows="8" cols="80" placeholder="Your complain"></textarea></td>
-                </tr>
-                <tr><td></td><td></td></tr>
-                <tr>
-                  <td></td>
-                  <td><button type="submit" class="log">Submit</button></td>
-                </tr>
-              </table>
-            </div>
-          </form>
+          <?php
+
+            while($data=mysql_fetch_array($result)) {
+            echo"<div class='admin-data'>";
+            echo $data['name'];
+            $empty=$data['name'];
+            echo "<a class='button view' href='message-view.php?id=$data[id]'>View</a>";
+            echo "</div>";
+
+          }
+            if (empty($empty)==true) {
+              $message = "You Have no Message !!";
+            }else{
+              $message = "You Have got some Message";
+
+            }
+
+
+          ?>
+
+
+          <br><br><br><br><br><br><br><br><br><br><br><br>
+
         </div>
       </div>
     </div>
+
+
       <footer>
-            <br><br>&copy <?php echo date("Y"); ?> <?php echo $web_name; ?>
+      <br><br>&copy <?php echo date("Y"); ?> <?php echo $web_name; ?>
       </footer>
-    <script src="files/js/jquery.js"></script>
-    <script src="files/js/bootstrap.min.js"></script>
-    <script src="files/js/script.js"></script>
+
+    <script src="../files/js/jquery.js"></script>
+    <script src="../files/js/bootstrap.min.js"></script>
+    <script src="../files/js/script.js"></script>
+
   </body>
 </html>
